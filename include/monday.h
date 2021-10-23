c8150258dfdff642a3f6ef0cb2af31d0a97b875d
Augustine129 <74011126+Augustine129@users.noreply.github.com>
2021-10-23 09:46:48 +0000

    Primer 3 completed with comments

--- include/monday.h
+++ include/monday.h
@@ -64,8 +64,46 @@ void fahrenheitCentigradeConversion(void) {
   std::cout << originTemperature << " degrees " << label << " is = " << t << "\n";
 }
 
-
 void selfServiceCheckout(void) {
-	std::cout << " - selfServiceCheckout: not yet implemented\n\n";
-}
+  int item = 0; //The number of itmes
+  int quantity; // The quantity of itmes
+  float shoppingTax = 0.0; //The total value of shopping tax
+  float subTotal = 0.0; //The total before taxes have been take away
+  const float tax = 5.5; //The total value of tax
+  float total = 0.0; //The total after taxes have been taken away
+  float cost = 0.0; // The toal cost of the item
+
+  do{
+      item++;// This increment the item by 1
+
+      //This ask the user to enter the items
+      std::cout<<"Please enter a quantity for item " << item <<" (or 0 to finish): \n";    
+      std::cin >> quantity;
+
+      if(quantity > 0) {
+        //sk the cost of the item
+      std::cout<<"Please enter item " << item <<" cost: \n";
+      std::cin >> cost;
+      subTotal = subTotal + (quantity * cost);
+      }    
+  }
+  while(quantity > 0);
+
+  //Try to Round to 2dp
+  subTotal = roundf(subTotal * 100) / 100;
+  shoppingTax = roundf(subTotal * (5.5 / 100) * 100) / 100;
+  total = roundf((subTotal + tax) * 100) / 100;
+  
+  std::cout<< "Thank you. \n";
+
+  std::cout<< "\nSubtotal: "<< subTotal;
+//The shoppingTax is calculated
+  shoppingTax = (subTotal * tax)/1000;
+  
+   std::cout << "Shopping Tax: " <<  tax << "\n\n";
+  total = subTotal + shoppingTax;
+  std::cout << "\nTotal: "<< std::fixed << total <<std::endl; 
+      
+};
+
 
